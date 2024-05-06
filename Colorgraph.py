425caa8dfb279a856a21f49721d59a7223386dc3
sanket patil <114555613+sanku712@users.noreply.github.com>
2024-05-06 17:23:31 +0530

    Update Colorgraph.py

--- Colorgraph.py
+++ Colorgraph.py
@@ -5,23 +5,16 @@ def addEdge(adj, v, w):
     adj[w].append(v)
     return adj
 
-# Assigns colors (starting from 0) to all
-# vertices and prints the assignment of colors
+
 def greedyColoring(adj, V):
     result = [-1] * V
 
     # Assign the first color to first vertex
     result[0] = 0
-
-    # A temporary array to store the available colors.
-    # True value of available[cr] would mean that the
-    # color cr is assigned to one of its adjacent vertices
     available = [False] * V
 
-    # Assign colors to remaining V-1 vertices
+    
     for u in range(1, V):
-        # Process all adjacent vertices and
-        # flag their colors as unavailable
         for i in adj[u]:
             if result[i] != -1:
                 available[result[i]] = True
